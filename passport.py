d1865e70bff537d502cab979d632ded1c6ee730e
ivanardo <ivanardoo@gmail.com>
2018-04-02 13:59:09 +0300

    Update passport.py
    исправил кодировку

--- passport.py
+++ passport.py
@@ -7,40 +7,40 @@ def relevanse(dob="14.2.1998", passport="1.1.2012"):
     dt_pas = datetime.strptime(passport, "%d.%m.%Y").date()
     today = date.today()
 
-    # возраст
+    # РІРѕР·СЂР°СЃС‚
     age = today.year - dt_dob.year
     if today.month < dt_dob.month or (today.month == dt_dob.month and today.day < dt_dob.day):
         age -= 1
 
-    # возраст при получении паспорта
+    # РІРѕР·СЂР°СЃС‚ РїСЂРё РїРѕР»СѓС‡РµРЅРёРё РїР°СЃРїРѕСЂС‚Р°
     get_pass_age = dt_pas.year - dt_dob.year
     if dt_pas.month < dt_dob.month or (dt_pas.month == dt_dob.month and dt_pas.day < dt_dob.day):
         get_pass_age -= 1
 
-    # Паспорт выдается в 14 лет и должен быть заменен в 20 и 45 лет, на замену паспорта дается 1 месяц.
+    # РџР°СЃРїРѕСЂС‚ РІС‹РґР°РµС‚СЃСЏ РІ 14 Р»РµС‚ Рё РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ Р·Р°РјРµРЅРµРЅ РІ 20 Рё 45 Р»РµС‚, РЅР° Р·Р°РјРµРЅСѓ РїР°СЃРїРѕСЂС‚Р° РґР°РµС‚СЃСЏ 1 РјРµСЃСЏС†.
     if 14 <= age < 20:
         if 14 <= get_pass_age < 20:
-            print("паспорт действителен")
+            print("РїР°СЃРїРѕСЂС‚ РґРµР№СЃС‚РІРёС‚РµР»РµРЅ")
             return True
         elif (today.year*12+today.month) - ((dt_dob.year+14)*12 + dt_dob.month) <= 1:
-            print("1 месяц на замену")
+            print("1 РјРµСЃСЏС† РЅР° Р·Р°РјРµРЅСѓ")
             return True
     elif 20 <= age < 45:
         if 20 <= get_pass_age < 45:
-            print("паспорт действителен")
+            print("РїР°СЃРїРѕСЂС‚ РґРµР№СЃС‚РІРёС‚РµР»РµРЅ")
             return True
         elif (today.year*12+today.month) - ((dt_dob.year+20)*12 + dt_dob.month) <= 1:
-            print("1 месяц на замену")
+            print("1 РјРµСЃСЏС† РЅР° Р·Р°РјРµРЅСѓ")
             return True
     elif age >= 45:
         if get_pass_age >= 45:
-            print("паспорт действителен")
+            print("РїР°СЃРїРѕСЂС‚ РґРµР№СЃС‚РІРёС‚РµР»РµРЅ")
             return True
         elif (today.year*12+today.month) - ((dt_dob.year+20)*12 + dt_dob.month) <= 1:
-            print("1 месяц на замену")
+            print("1 РјРµСЃСЏС† РЅР° Р·Р°РјРµРЅСѓ")
             return True
 
-    print("паспорт недействителен")
+    print("РїР°СЃРїРѕСЂС‚ РЅРµРґРµР№СЃС‚РІРёС‚РµР»РµРЅ")
     return False
 
 
